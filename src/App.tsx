5d7ebdbd094a401fdfb7657a8f30b4bb88a7f104
mjn81 <mjjnn81@gmail.com>
2022-11-25 23:13:58 +0330

    [INIT] .

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="text-7xl text-red-500 flex justify-center items-center w-screen h-screen">
+      Test
+    </div>
+  )
+}
+
+export default App
